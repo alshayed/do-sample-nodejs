b1d7c7ebe7b44aa94abab5286cfde534fafc4345
Brandon Peterson <brandon@thebandit.org>
2022-09-24 21:59:22 -0600

    add mongoose db connection but it won't work

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
+const mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  mongoose.connect('mongodb://localhost:27017/test');
+
+  const Cat = mongoose.model('Cat', { name: String });
+
+  const kitty = new Cat({ name: 'Zildjian' });
+  kitty.save().then(() => console.log('meow'));
+
   res.send('respond with a resource');
 });
 
